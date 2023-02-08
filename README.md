299698f2c09a096c2d0de52cb092ab012a4d4d7e
Daniel Nguyen <mduongbkhn@gmail.com>
2023-02-08 17:39:01 +0900

    readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # CVAE-Pytorch
  
+# Clone
+```
+git clone https://github.com/Skydvn/CVAE-Pytorch.git
+```
+
+# Training
+```commandline
+python CVAE_main.py --encoder dense -- en_size 5 --de_size 5 --epochs 5 --lr 0.005 --wd 0.001
+```
--- models/loss.py
+++ models/loss.py
@@ -1,6 +1,14 @@
 import torch
+import torch.nn.functional as F
 
 
-def loss_fn(x, y):
-    pass
+def loss_fn(recon_x, x):
+    BCE = F.binary_cross_entropy(recon_x.view(-1, 28 * 28), x.view(-1, 28 * 28), reduction=sum)
+    return BCE / x.size(0)
 
+# def loss_fn(recon_x, x, mean, log_var):
+#     BCE = torch.nn.functional.binary_cross_entropy(
+#         recon_x.view(-1, 28 * 28), x.view(-1, 28 * 28), reduction='sum')
+#     KLD = -0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp())
+#
+#     return (BCE + KLD) / x.size(0)
