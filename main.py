da61fd7eac03843ac1bd0bc66e1c25b2fc01053b
Daniel Nguyen <mduongbkhn@gmail.com>
2023-02-10 23:43:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,13 @@
 from test_manager import *
 from train_manager import *
 from utils.setting_setup import *
+import torch
+
 
 if __name__ == "__main__":
     # get parser
     args = get_arguments()
+    print(torch.cuda.is_available())
 
     if args.mode == "train":
         train(args)
